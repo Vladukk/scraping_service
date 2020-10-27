612509ac5f512430dfc7983af4a00cd5f819f790
Vlad <vladu_k@mail.ru>
2020-10-27 22:01:12 +0300

    Second Commit

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Vacancy
+
+def home_view(requst):
+    qs = Vacancy.objects.all()
+    return  render(requst, 'scraping/home.html', {'object_list': qs})
+
 # Create your views here.
